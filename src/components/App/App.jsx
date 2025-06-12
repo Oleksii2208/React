562d4ebf4fa87f5790c7b82e74fe299f8ccb8142
Oleksiy Kost <AlekseyKo2208@gmail.com>
2025-06-12 09:35:31 +0300

    app

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,7 +1,11 @@
-import React from "react";
+import s from "./App.module.css";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div className={s.section}>
+      <h1 className={s.title}>App</h1>
+    </div>
+  );
 };
 
 export default App;
